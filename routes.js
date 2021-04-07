4d6f70d7749f86555841ce5a10f26efd8934751a
Mikhail <lmnyx@act8team.com>
2021-04-07 18:19:50 +0500

    change text of blog post listing

--- routes.js
+++ routes.js
@@ -54,7 +54,7 @@ const Routes =
 		else
 			_pageID = req.params.page_id;
 		_blog = await utils.GetBlog(_pageID);
-		return res.view(__dirname+"/layouts/blog.ejs", { "posts": _blog });
+		return res.view(__dirname+"/layouts/blog.ejs", { "posts": _blog, "page": _pageID+1 });
 	},
 	"blogpost": async (req, res)=>
 	{
