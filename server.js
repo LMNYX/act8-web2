3923e921742e9ff33df5015846104b22fa4967dd
Mikhail <lmnyx@act8team.com>
2021-05-13 20:08:18 +0500

    make commits available for public view

--- server.js
+++ server.js
@@ -48,8 +48,8 @@ fastify.get('/game/:game_id', routes.game);
 fastify.get('/avatar/:user', routes.avatarprocessor);
 fastify.get('/poster/:game', routes.posterprocessor);
 
-//fastify.get('/commits', routes.commits);
-//fastify.get('/commits/:page_id', routes.commits);
+fastify.get('/commits', routes.commits);
+fastify.get('/commits/:page_id', routes.commits);
 
 /* GitHub */
 fastify.post('/git-commit-listener', routes.gitCommitListener);
