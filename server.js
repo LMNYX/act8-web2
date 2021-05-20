bf3ba75a5496e9c84c034b9d87a507df0a16633f
Mikhail <lmnyx@act8team.com>
2021-05-20 19:20:58 +0500

    disable commits for the purpose of release

--- server.js
+++ server.js
@@ -48,9 +48,6 @@ fastify.get('/game/:game_id', routes.game);
 fastify.get('/avatar/:user', routes.avatarprocessor);
 fastify.get('/poster/:game', routes.posterprocessor);
 
-fastify.get('/commits', routes.commits);
-fastify.get('/commits/:page_id', routes.commits);
-
 /* GitHub */
 fastify.post('/git-commit-listener', routes.gitCommitListener);
 fastify.post('/git-commit-accepter', routes.githubCommitAccepter);
